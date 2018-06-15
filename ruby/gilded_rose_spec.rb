c4ec43dae87885cdd88f36cbab86f0f76f394d93
Omar De Los Santos <omarnyte@gmail.com>
2018-06-15 14:55:01 -0700

    Write #initialize specs

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,3 +1,4 @@
+
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
@@ -6,8 +7,16 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
     end
   end
+
+  describe "initialize" do 
+    it "requires that all items have a @sell_in value and @quality" do       
+      expect{Item.new("foo")}.to raise_error(ArgumentError)
+      expect{Item.new("foo", 10)}.to raise_error(ArgumentError)
+      expect{Item.new("foo", 10, 10)}.to_not raise_error
+    end
+  end 
 
 end

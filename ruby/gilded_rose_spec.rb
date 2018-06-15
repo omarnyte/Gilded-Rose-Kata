bc763f7f49b0a8aa8b8e5bd483be2d0263636259
Omar De Los Santos <omarnyte@gmail.com>
2018-06-15 14:55:35 -0700

    Write and pass tests for original functionality

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,4 +1,3 @@
-
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
@@ -11,7 +10,7 @@ describe GildedRose do
     end
   end
 
-  describe "initialize" do 
+  describe "Item initialize" do 
     it "requires that all items have a @sell_in value and @quality" do       
       expect{Item.new("foo")}.to raise_error(ArgumentError)
       expect{Item.new("foo", 10)}.to raise_error(ArgumentError)
@@ -19,4 +18,66 @@ describe GildedRose do
     end
   end 
 
+  describe "#update_quality" do
+    it "lowers both @sell_in and @quality for every item at the end of each day" do 
+      items = [Item.new("foo", 10, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq(9)
+      expect(items[0].quality).to eq(9)
+    end 
+
+    it "degreades quality twice as fast once the sell date has passed" do 
+      items = [Item.new("foo", 0, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq(-1)
+      expect(items[0].quality).to eq(8)
+    end 
+
+    it "never brings down quality to a negative value" do 
+      items = [Item.new("foo", 10, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq(9)
+      expect(items[0].quality).to eq(0)
+    end 
+
+    it "increases the quality of 'Aged Brie' the older it gets" do 
+      items = [Item.new("Aged Brie", 10, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq(9)
+      expect(items[0].quality).to eq(11)
+    end 
+
+    it "never incrases the quality of an item to be more than 50" do 
+      items = [Item.new("Aged Brie", 10, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq(9)
+      expect(items[0].quality).to eq(50)
+    end 
+
+    it "never decereases @sell_in or @quality for 'Sulfuras'" do 
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 80)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq(10)
+      expect(items[0].quality).to eq(80)
+    end 
+
+    it "handles the incrase of 'Backstage passes to a TAFKAL80ETC concert'" do 
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 6, 10)]
+      gr = GildedRose.new(items)
+      gr.update_quality
+      expect(items[0].sell_in).to eq(5)
+      expect(items[0].quality).to eq(12)
+
+      gr.update_quality
+      expect(items[0].sell_in).to eq(4)
+      expect(items[0].quality).to eq(15)
+
+      5.times do 
+        gr.update_quality
+      end 
+
+      expect(items[0].sell_in).to eq(-1)
+      expect(items[0].quality).to eq(0)
+    end     
+  end 
 end
